48c616869c1011d0cd06b00b7ae852e0ea48ed5c
wei.mao <wei.mao@jimubox.com>
2016-10-25 17:08:34 +0800

    using coredata to store

--- HitList/HitList/ViewController.swift
+++ HitList/HitList/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
-    var names: [String] = []
+//    var names: [String] = []
+    var people: [NSManagedObject] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,7 +43,9 @@ class ViewController: UIViewController {
                                                 return
                                         }
                                         
-                                        self.names.append(nameToSave)
+//                                        self.names.append(nameToSave)
+                                        self.save(name: nameToSave)
+
                                         self.tableView.reloadData()
         }
         
@@ -55,25 +59,76 @@ class ViewController: UIViewController {
         
         present(alert, animated: true)
     }
+    
+    func save(name: String) {
+        
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                return
+        }
+        
+        // 1
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Person",
+                                       in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity,
+                                     insertInto: managedContext)
+        
+        // 3
+        person.setValue(name, forKeyPath: "name")
+        
+        // 4
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
 
 }
 
+//// MARK: - UITableViewDataSource
+//extension ViewController: UITableViewDataSource {
+//    
+//    func tableView(_ tableView: UITableView,
+//                   numberOfRowsInSection section: Int) -> Int {
+//        return names.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView,
+//                   cellForRowAt indexPath: IndexPath)
+//        -> UITableViewCell {
+//            
+//            let cell =
+//                tableView.dequeueReusableCell(withIdentifier: "Cell",
+//                                              for: indexPath)
+//            cell.textLabel?.text = names[indexPath.row]
+//            return cell
+//    }
+//}
 // MARK: - UITableViewDataSource
 extension ViewController: UITableViewDataSource {
-    
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath)
         -> UITableViewCell {
             
+            let person = people[indexPath.row]
             let cell =
                 tableView.dequeueReusableCell(withIdentifier: "Cell",
                                               for: indexPath)
-            cell.textLabel?.text = names[indexPath.row]
+            cell.textLabel?.text =
+                person.value(forKeyPath: "name") as? String
             return cell
     }
 }
